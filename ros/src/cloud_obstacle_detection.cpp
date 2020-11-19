bb03a831a2f1ea86511c87d69e90dd0d13d12b5f
Sushant Chavan <gitecsvc@gmail.com>
2020-11-19 22:00:17 +0100

    Implement obstacle cache

--- ros/src/cloud_obstacle_detection.cpp
+++ ros/src/cloud_obstacle_detection.cpp
@@ -119,17 +119,30 @@ private:
     ros::Publisher mObstacleCloudPub;
     ros::Publisher mMarkerPub;
     tf::TransformListener mTfListener;
-    std::string mTargetFrame;
+    std::string mTransformTargetFrame;
+    std::string mClusterTargetFrame;
     CloudPassThroughVoxelFilter mCloudFilter;
     EuclideanClusterParams mClusterParams;
+
+    double mSimilarityThreshold;
+    double mUniquenessThreshold;
+    unsigned int mPositionCacheLimit;
+    unsigned int mUniqueObstacleId;
+    float mObstacleCacheTime;
+    unsigned int mCurrTime;
+    std::map<int, int> mLastSeenTimeCache;
+    std::map<int, const PointCloud::Ptr> mObstaclesCache;
+    std::map<int, std::vector<Eigen::Vector4f>> mPrevPositionsCache;
 
 public:
     CloudObstacleDetectionNode(const ros::NodeHandle &pNodeHandle, const std::string &pCloudTopic,
             const std::string &pProcessedCloudTopic, 
-            const std::string &pObstacleCloudTopic, 
-            const std::string &pTargetFrame, 
+            const std::string &pObstacleCloudTopic,
+            const std::string &pTransformTargetFrame,
+            const std::string &pClusterTargetFrame,
             const std::string &pObstaclesDetectionTopic)
-    : mNodeHandle(pNodeHandle), mObstacleDetectionConfigServer(mNodeHandle), mTargetFrame(pTargetFrame)
+    : mNodeHandle(pNodeHandle), mObstacleDetectionConfigServer(mNodeHandle), 
+      mTransformTargetFrame(pTransformTargetFrame), mClusterTargetFrame(pClusterTargetFrame)
     {
         ROS_INFO("setting up dynamic reconfiguration server for obstacle detection");
         auto odCallback = boost::bind(&CloudObstacleDetectionNode::obstacleDetectionConfigCallback, this, _1, _2);
@@ -159,10 +172,16 @@ private:
         cloudFilterParams.mVoxelLeafSize = static_cast<float>(pConfig.voxel_leaf_size);
         mCloudFilter.setParams(cloudFilterParams);
 
-        // CLoud CLustering params
+        // Cloud Clustering params
         mClusterParams.mClusterTolerance = static_cast<float>(pConfig.cluster_tolerance);
         mClusterParams.mMinClusterSize = static_cast<unsigned int>(pConfig.min_cluster_size);
         mClusterParams.mMaxClusterSize = static_cast<unsigned int>(pConfig.max_cluster_size);
+
+        // Obstacle cache params
+        mObstacleCacheTime = static_cast<unsigned int>(pConfig.obstacle_cache_time);
+        mSimilarityThreshold = static_cast<float>(pConfig.similarity_threshold);
+        mUniquenessThreshold = static_cast<float>(pConfig.uniqueness_threshold);
+        mPositionCacheLimit = static_cast<float>(pConfig.position_history_cache_size);
     }
 
     void
@@ -176,10 +195,10 @@ private:
 
         // transform the cloud to a desired frame
         auto transformedCloudPtr = boost::make_shared<sensor_msgs::PointCloud2>();
-        if (!pcl_ros::transformPointCloud(mTargetFrame, *pCloudMsgPtr, *transformedCloudPtr, mTfListener))
+        if (!pcl_ros::transformPointCloud(mTransformTargetFrame, *pCloudMsgPtr, *transformedCloudPtr, mTfListener))
         {
             ROS_WARN("failed to transform cloud to frame '%s' from frame '%s'",
-                     mTargetFrame.c_str(), pCloudMsgPtr->header.frame_id.c_str());
+                     mTransformTargetFrame.c_str(), pCloudMsgPtr->header.frame_id.c_str());
             return;
         }
 
@@ -187,6 +206,15 @@ private:
         PointCloud::Ptr pclCloudPtr = boost::make_shared<PointCloud>();
         pcl::fromROSMsg(*transformedCloudPtr, *pclCloudPtr);
         PointCloud::Ptr filteredCloudPtr = mCloudFilter.filterCloud(pclCloudPtr);
+
+        if (filteredCloudPtr->size() < mClusterParams.mMinClusterSize)
+        {
+            // Stop processing the point cloud if the filtered cloud does 
+            // not have enough points to create even one cluster
+            return;
+        }
+
+        mCurrTime = pCloudMsgPtr->header.stamp.sec;
 
         if (mFilteredCloudPub.getNumSubscribers() > 0)
         {
@@ -198,35 +226,35 @@ private:
         pcl::search::Search<PointT>::Ptr tree(new pcl::search::KdTree<PointT>);
         tree->setInputCloud(filteredCloudPtr);
         std::vector<pcl::PointIndices> cluster_indices;
+        std::vector<PointCloud::Ptr> clusterClouds;
         pcl::extractEuclideanClusters(*filteredCloudPtr,
                                       tree,
                                       mClusterParams.mClusterTolerance,
                                       cluster_indices,
                                       mClusterParams.mMinClusterSize,
                                       mClusterParams.mMaxClusterSize);
+        getClusterClouds(clusterClouds, filteredCloudPtr, cluster_indices);
 
-        if (mMarkerPub.getNumSubscribers() > 0)
-        {
-            publishClusterMarkers(filteredCloudPtr, cluster_indices);
-        }
+        processNewClusters(clusterClouds);
+        removeStaleObstacles();
 
-        if (mObstacleCloudPub.getNumSubscribers() > 0)
-        {
-            publishObstacleCloud(filteredCloudPtr, cluster_indices);
-        }
+        publishObstacleCloud();
+        publishClusterMarkers();
     }
 
-    void 
-    publishClusterMarkers(PointCloud::ConstPtr filteredCloud, 
-                          const std::vector<pcl::PointIndices>& cluster_indices)
+    void
+    publishClusterMarkers()
     {
+        if (mMarkerPub.getNumSubscribers() <= 0)
+            return;
+
         visualization_msgs::MarkerArray markerArray;
-        int i = 0;
-        for (const auto& indices: cluster_indices)
+        for (const auto& c : mObstaclesCache)
         {
             Eigen::Vector4f min, max;
-            pcl::getMinMax3D(*filteredCloud, indices, min, max);
-            markerArray.markers.push_back(getMarker(min, max, i++));
+            pcl::getMinMax3D(*(c.second), min, max);
+            markerArray.markers.push_back(getMarker(min, max, c.first));
+            markerArray.markers.push_back(getTextLabel(min, max, c.first));
         }
         // Publish the marker array
         mMarkerPub.publish(markerArray);
@@ -248,8 +276,8 @@ private:
         visualization_msgs::Marker marker;
         marker.type = visualization_msgs::Marker::LINE_LIST;
         marker.action = visualization_msgs::Marker::ADD;
-        marker.lifetime = ros::Duration(2.0);
-        marker.header.frame_id = mTargetFrame;
+        marker.lifetime = ros::Duration(1.0);
+        marker.header.frame_id = mTransformTargetFrame;
         marker.scale.x = 0.005;
         marker.scale.y = 0.005;
         marker.color.a = 2.0;
@@ -267,18 +295,173 @@ private:
         return marker;
     }
 
-    void
-    publishObstacleCloud(PointCloud::ConstPtr filteredCloud, 
-                         const std::vector<pcl::PointIndices>& cluster_indices)
+    visualization_msgs::Marker 
+    getTextLabel(const Eigen::Vector4f& min, const Eigen::Vector4f& max, int id)
     {
-        PointCloud::Ptr mergedCloud(new PointCloud);
+        Eigen::Vector4f center = min + (max - min)/2.0;
+        visualization_msgs::Marker marker;
+        marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+        marker.text = std::to_string(id);
+        marker.lifetime = ros::Duration(1.0);
+        marker.header.frame_id = mTransformTargetFrame;
+        marker.pose.position.x = center[0];
+        marker.pose.position.y = center[1];
+        marker.pose.position.z = center[2];
+        marker.scale.z = 0.05;
+        marker.id = id + 1000;
+        marker.color = std_msgs::ColorRGBA(Color(Color::SCARLET));
+        return marker;
+    }
+
+    void getClusterClouds(std::vector<PointCloud::Ptr>& clusterClouds,
+                          PointCloud::ConstPtr filteredCloud, 
+                          const std::vector<pcl::PointIndices>& cluster_indices)
+    {
         for (const auto& idx: cluster_indices)
         {
+            PointCloud::Ptr clusterCloud(new PointCloud);
             for (const auto &index : idx.indices)
-                mergedCloud->push_back ((*filteredCloud)[index]); 
+                clusterCloud->push_back ((*filteredCloud)[index]);
+
+            clusterCloud->header = filteredCloud->header;
+            clusterCloud->width = clusterCloud->size();
+            clusterCloud->height = 1;
+            clusterCloud->is_dense = true;
+
+            PointCloud::Ptr transformedCloudPtr = boost::make_shared<PointCloud>();
+            pcl_ros::transformPointCloud(mClusterTargetFrame, *clusterCloud, *transformedCloudPtr, mTfListener);
+
+            clusterClouds.push_back(transformedCloudPtr);
         }
-        mergedCloud->header = filteredCloud->header;
-        mergedCloud->width = mergedCloud->size ();
+    }
+
+    // Cloud comparison sample from https://stackoverflow.com/a/55930847
+    float nearestDistance(const pcl::search::KdTree<PointT>& tree, const PointT& pt)
+    {
+        const int k = 1;
+        std::vector<int> indices (k);
+        std::vector<float> sqr_distances (k);
+
+        tree.nearestKSearch(pt, k, indices, sqr_distances);
+
+        return sqr_distances[0];
+    }
+    // compare cloudB to cloudA
+    // use threshold for identifying outliers and not considering those for the similarity
+    // a good value for threshold is 5 * <cloud_resolution>, e.g. 10cm for a cloud with 2cm resolution
+    float getCloudSimilarity(const PointCloud& cloudA, const PointCloud& cloudB, float threshold)
+    {
+        // compare B to A
+        int num_outlier = 0;
+        pcl::search::KdTree<PointT> tree;
+        tree.setInputCloud(cloudA.makeShared());
+        auto sum = std::accumulate(cloudB.begin(), cloudB.end(), 0.0f, [&](float current_sum, const PointT& pt) {
+            const auto dist = nearestDistance(tree, pt);
+
+            if(dist < threshold)
+            {
+                return current_sum + dist;
+            }
+            else
+            {
+                num_outlier++;
+                return current_sum;
+            }
+        });
+
+        return sum / (cloudB.size() - num_outlier);
+    }
+
+    bool 
+    isNewObstacle(const PointCloud& cloud, int& knownObstacleId)
+    {
+        for (const auto& o : mObstaclesCache)
+        {
+            if (getCloudSimilarity(cloud, *(o.second), mSimilarityThreshold) < mUniquenessThreshold)
+            {
+                knownObstacleId = o.first;
+                return false;
+            }
+        }
+        knownObstacleId = -1;
+        return true;
+    }
+
+    void
+    processNewClusters(const std::vector<PointCloud::Ptr>& clusterClouds)
+    {
+        for (const auto& cloud : clusterClouds)
+        {
+            Eigen::Vector4f centroid;
+            if (pcl::compute3DCentroid(*cloud, centroid) <= 0)
+            {
+                ROS_WARN("Could not compute centroid of point cloud. Skipping processing of potential obstacle cluster!");
+                return;
+            }
+
+            int obstacleID = -1;
+            if (isNewObstacle(*cloud, obstacleID))
+            {
+                obstacleID = mUniqueObstacleId++;
+                ROS_INFO("Adding new obstacle: %d", obstacleID);
+                mObstaclesCache.insert(std::pair<int, const PointCloud::Ptr>(obstacleID, cloud));
+                std::vector<Eigen::Vector4f> prevPositions;
+                prevPositions.push_back(centroid);
+                mPrevPositionsCache.insert(std::pair<int, std::vector<Eigen::Vector4f>>(obstacleID, prevPositions));
+                mLastSeenTimeCache.insert(std::pair<int, int>(obstacleID, mCurrTime));
+            }
+            else
+            {
+                // update last known position
+                std::vector<Eigen::Vector4f>& prevPositions = mPrevPositionsCache[obstacleID];
+                if (prevPositions.size() > mPositionCacheLimit)
+                {
+                    prevPositions.erase(prevPositions.begin());
+                }
+                prevPositions.push_back(centroid);
+
+                mLastSeenTimeCache[obstacleID] = mCurrTime;
+            }
+        }
+    }
+
+    void removeStaleObstacles()
+    {
+        for (auto it = mLastSeenTimeCache.cbegin(); it != mLastSeenTimeCache.cend(); )
+        {
+            int id = it->first;
+            if (mCurrTime - it->second > mObstacleCacheTime)
+            {
+                ROS_INFO("Removing Stale obstacle: %d", id);
+                mLastSeenTimeCache.erase(it++);
+                mPrevPositionsCache.erase(id);
+                mObstaclesCache.erase(id);
+            }
+            else
+            {
+                ++it;
+            }
+        }
+    }
+
+    void
+    publishObstacleCloud()
+    {
+        if (mObstacleCloudPub.getNumSubscribers() <= 0)
+            return;
+
+        // Merge all the clusters into once cloud
+        PointCloud::Ptr mergedCloud(new PointCloud);
+        bool headerInitialized = false;
+        for (const auto& c : mObstaclesCache)
+        {   if (!headerInitialized)
+            {
+                mergedCloud->header = c.second->header;
+                headerInitialized = true;
+            }
+            *mergedCloud += *(c.second);
+        }
+        mergedCloud->width = mergedCloud->size();
         mergedCloud->height = 1;
         mergedCloud->is_dense = true;
 
@@ -304,7 +487,8 @@ int main(int pArgc, char** pArgv)
     ros::NodeHandle nh("~");
 
     // load launch parameters
-    std::string cloudTopic, processedCloudTopic, obstacleCloudTopic, obstacleDetectionsTopic, targetFrame;
+    std::string cloudTopic, processedCloudTopic, obstacleCloudTopic, 
+                obstacleDetectionsTopic, transformTargetFrame, clusterTargetFrame;
     if (!nh.getParam("cloud_topic", cloudTopic) || cloudTopic.empty())
     {
         ROS_ERROR("No 'cloud_topic' specified as parameter");
@@ -325,15 +509,20 @@ int main(int pArgc, char** pArgv)
         ROS_ERROR("No 'obstacles_detection_topic' specified as parameter");
         return EXIT_FAILURE;
     }
-    if (!nh.getParam("target_frame", targetFrame) || targetFrame.empty())
+    if (!nh.getParam("transform_target_frame", transformTargetFrame) || transformTargetFrame.empty())
     {
-        ROS_ERROR("No 'target_frame' specified as parameter");
+        ROS_ERROR("No 'transform_target_frame' specified as parameter");
+        return EXIT_FAILURE;
+    }
+    if (!nh.getParam("cluster_target_frame", clusterTargetFrame) || clusterTargetFrame.empty())
+    {
+        ROS_ERROR("No 'cluster_target_frame' specified as parameter");
         return EXIT_FAILURE;
     }
 
     // run cloud filtering and obstacle detection
     mas_perception_libs::CloudObstacleDetectionNode obstacleDetection(nh, cloudTopic, processedCloudTopic, obstacleCloudTopic,
-                                                                         targetFrame, obstacleDetectionsTopic);
+                                                                         transformTargetFrame, clusterTargetFrame, obstacleDetectionsTopic);
 
     while (ros::ok())
         ros::spin();
